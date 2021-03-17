6612a51a98bd3b31737d5a90cdfaa363451b6d41
liaaaaran <ran275@umail.ucsb.edu>
2021-03-17 04:32:59 -0700

    Added new dataset (time series going till 2015) and finished MCR vignette

--- R/mcr_coral_doc.R
+++ R/mcr_coral_doc.R
@@ -6,8 +6,10 @@
 #' Branch morphology on a scale from 1 -> 3 with tight -> open spacing
 #'
 #' @source {MCR LTER: Coral Reef Resilience: Live and Dead Pocillopora and Acropora Coral Colony Time Series from 2006 to 2011}
+#' @source {MCR LTER: Coral Reef: Long-term Population and Community Dynamics: Corals, ongoing since 2005}
 #'
 #' \url{Moorea Coral Reef LTER, T. Adam, A. Brooks, R. Schmitt, and S. Holbrook. 2019. MCR LTER: Coral Reef Resilience: Live and Dead Pocillopora and Acropora Coral Colony Time Series from 2006 to 2011 ver 2. Environmental Data Initiative. https://doi.org/10.6073/pasta/23157cfdedcc6522c999e593d83007c9 (Accessed 2021-03-01).}
+#'  \url{Moorea Coral Reef LTER and P. Edmunds. 2015. MCR LTER: Coral Reef: Long-term Population and Community Dynamics: Corals, ongoing since 2005 ver 33. Environmental Data Initiative. https://doi.org/10.6073/pasta/1f05f1f52a2759dc096da9c24e88b1e8 (Accessed 2021-03-12).}
 
 
 "mcr_coral"
--- data-raw/mcr_coral.R
+++ data-raw/mcr_coral.R
@@ -9,17 +9,22 @@ library(tidyverse)
 
 # save link location
 mcr_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-mcr.7010.2&entityid=eaa8964320aee040e573569057e6c74c"
+mcr_url1 <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-mcr.4.33&entityid=7d9ac9398cb2ae30e7c3f224fb489eff"
 
 # download package with metajam
 mcr_download <- download_d1_data(data_url = mcr_url, path = tempdir())
+mcr_download1 <- download_d1_data(data_url = mcr_url1, path = tempdir())
 
 # read in data
 mcr_files <- read_d1_files(mcr_download)
 mcr_coral <- mcr_files$data
 
+mcr_files1 <- read_d1_files(mcr_download1)
+mcr_coral1 <- mcr_files1$data
 # possible data cleaning can go here:
 mcrcoral <- mcr_coral %>%
   select(-c('Notes')) %>%
 
 
 usethis::use_data(mcr_coral, overwrite = TRUE)
+usethis::use_data(mcr_coral1, overwrite = TRUE)
